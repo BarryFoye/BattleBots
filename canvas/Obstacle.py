0bd16069e9c9cda9d2ff0a57bec15c0079777c10
BarryFoye <barry.foye@gmail.com>
2019-04-30 21:29:45 +0100

    Added obstacles

---
+++ canvas/Obstacle.py
@@ -0,0 +1,20 @@
+class Obstacle:
+    position = 0
+    rotation = 0
+    bignessX = 0
+    bignessY = 0
+    
+    def __init__(self):
+        self.position = PVector(floor(random(0, 280)), floor(random(0, 280)))
+        self.rotation = random(0, TWO_PI)
+        self.bignessX = floor(random(20, 60))
+        self.bignessY = floor(random(20, 60))
+    
+    def show(self):
+        
+        pushMatrix();
+        fill(0)
+        translate(self.position.x, self.position.y)        
+        rotate(self.rotation)
+        rect(0, 0, self.bignessX, self.bignessY)
+        popMatrix()
--- canvas/World.py
+++ canvas/World.py
@@ -1,15 +1,19 @@
 class World:
     from Food import Food
+    from Obstacle import Obstacle
     width_ = 0
     height_ = 0     
     food = 0
+    obstacle = 0
     
     def __init__(self, w, h):
         id = 0;
         self.width_ = w
         self.height_ = h
         self.food = self.Food() 
+        self.obstacle = self.Obstacle()
 
                 
     def run(self):
         self.food.show()
+        self.obstacle.show()
