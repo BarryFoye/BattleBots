0ebbad8caffffe15a95afc186a00a2d4deddc45c
BarryFoye <barry.foye@gmail.com>
2019-04-30 22:18:18 +0100

    added arrays of food and obstacles

--- canvas/World.py
+++ canvas/World.py
@@ -3,17 +3,23 @@ class World:
     from Obstacle import Obstacle
     width_ = 0
     height_ = 0     
-    food = 0
-    obstacle = 0
+    food = []
+    obstacle = []
     
-    def __init__(self, w, h):
+    def __init__(self, w, h, pop_food, pop_obstacles):
         id = 0;
         self.width_ = w
         self.height_ = h
-        self.food = self.Food() 
-        self.obstacle = self.Obstacle()
+        for i in range(0, pop_food):
+            self.food.append(self.Food()) 
+        
+        for i in range(0, pop_obstacles):
+            self.obstacle.append(self.Obstacle())
 
                 
     def run(self):
-        self.food.show()
-        self.obstacle.show()
+        for i in range(0, len(self.food)):
+            self.food[i].show()
+            
+        for i in range(0, len(self.obstacle)):
+            self.obstacle[i].show()
