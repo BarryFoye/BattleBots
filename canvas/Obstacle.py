5938e3cf9f4e27150fb36ab6474ea454dc3d2cf2
BarryFoye <barry.foye@gmail.com>
2019-05-12 21:15:11 +0100

    working taboo feature

--- canvas/Obstacle.py
+++ canvas/Obstacle.py
@@ -4,17 +4,74 @@ class Obstacle:
     bignessX = 0
     bignessY = 0
     
+    ax = 0
+    ay = 0
+    bx = 0
+    by = 0
+    cx = 0
+    cy = 0
+    dx = 0
+    dy = 0
+    
+    
     def __init__(self):
         self.position = PVector(floor(random(120, 280)), floor(random(120, 280)))#120 and 280 means th food will fall within a border 120 pix insids the screen
         self.rotation = random(0, TWO_PI)
         self.bignessX = floor(random(20, 60))
         self.bignessY = floor(random(20, 60))
     
-    def show(self):
-        
+    def show(self):        
         pushMatrix();
-        fill(0)
+        noStroke()
+        fill(0, 200)
         translate(self.position.x, self.position.y)        
         rotate(self.rotation)
         rect(0, 0, self.bignessX, self.bignessY)
         popMatrix()
+        stroke(255)
+        ellipse(self.ax, self.ay, 2, 2)
+        stroke(255, 0 , 0)
+        ellipse(self.bx, self.by, 2, 2)
+        stroke(0, 255, 0)
+        ellipse(self.cx, self.cy, 2, 2)
+        stroke(0, 0, 255)
+        ellipse(self.dx, self.dy, 2, 2)
+        #
+        newx = self.ax * cos(self.rotation) - self.ay * sin(self.rotation)
+        newy = self.ax * sin(self.rotation) + self.ay * cos(self.rotation)
+        stroke(50, 50, 50)
+        ellipse(newx, newy, 4, 4)
+        line(self.ax, self.ay, newx, newy)
+        print(dist(self.ax, self.ay, newx, newy))
+        print(dist(self.ax, self.ay, self.bx, self.by))
+        #
+        #
+        #
+        #
+        #
+        
+    
+    def add_taboo(self):
+        # calculate the area of the object
+        area = self.bignessX * self.bignessY
+        # calculate the position of each corner
+        self.ax = self.position.x
+        self.ay = self.position.y
+        self.bx = self.ax + self.bignessX
+        self.by = self.ay
+        self.cx = self.ax
+        self.cy = self.ay + self.bignessY
+        self.dx = self.ax + self.bignessX
+        self.dy = self.ay + self.bignessY
+        print("bignessX & bignessY = {} {}".format(self.bignessX, self.bignessY))
+        print("area = bignessX * bignessY = {}".format(area))
+        print("ax = {}".format(self.ax))
+        print("ay = {}".format(self.ay))
+        print("bx = ax + self.bignessX = {}".format(self.bx))
+        print("by = ay = {}".format(self.by))        
+        print("cx = ax = {}".format(self.cx))
+        print("cy = ay + self.bignessY = {}".format(self.cy))
+        print("dx = ax + self.bignessX = {}".format(self.dx))
+        print("dy = ay + self.bignessY = {}".format(self.dy))
+
+        
--- canvas/canvas.pyde
+++ canvas/canvas.pyde
@@ -2,7 +2,7 @@ from World import World
 w = 400
 h = 400
 
-world = World(w, h, 2, 2, 2)
+world = World(w, h, 0, 1, 0)
 def setup():
     size(w, h)
     
