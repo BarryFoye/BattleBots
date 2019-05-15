28c643f03f459f49e97873d69390d9afe9421645
Peter <User@DESKTOP-D3QMVG5>
2019-05-15 21:23:08 +0100

    MOVEMENT WORKS PROPERLY NOW.

--- canvas/Vehicle.py
+++ canvas/Vehicle.py
@@ -13,7 +13,7 @@ class Vehicle:
     
     #Configuration Variables
     mass = 5
-    max_speed = 0.0
+    max_speed = 0.5
     max_acceleration_rate = 1.0
     max_angular_rate = 0.1
     vision_radius = 0
@@ -36,10 +36,8 @@ class Vehicle:
         #Store dimensions of the world
         self.world = world
         #Initialise to a random position
-        #x = random.random() * self.world.width_
-        #y = random.random() * self.world.height_
-        x = 50.0
-        y = 100.0
+        x = random.random() * self.world.width_
+        y = random.random() * self.world.height_
         #self.current_angle = random.random() * 2 * PI
         self.current_position = PVector(x,y)
         self.current_velocity = PVector(0.0,0.0)
@@ -73,17 +71,19 @@ class Vehicle:
         self.apply_velocity()
         #
         #Hard limits
-        #self.checkBorders()
+        self.checkBorders()
+        #
+        #
                    
     def calculate_rotation_required(self):
         current_angle = self.current_angle
-        desired_angle = PVector.angleBetween(self.current_position, self.current_target)
+        desired_angle = atan2((self.current_target.y - self.current_position.y),(self.current_target.x - self.current_position.x))
+        println("DesiredAngle: " + str(desired_angle) + " CurrentAngle: " + str(self.current_angle))
         return desired_angle - current_angle
     
-    def apply_rotation(self,rotation_required):
-        #println("DesiredAngle: " + str(desired_angle) + " CurrentAngle: " + str(self.current_angle))
+    def apply_rotation(self,rotation_required):       
         if rotation_required > self.max_angular_rate:
-            rotation = max_angular_rate
+            rotation = self.max_angular_rate
         elif rotation_required < (-1 * self.max_angular_rate):
             rotation = (-1 * self.max_angular_rate)
         else:
@@ -91,11 +91,7 @@ class Vehicle:
         self.add_angle(rotation)
 
     def add_angle(self,angle):
-        self.current_angle += angle
-        while self.current_angle >= (2*PI):
-            self.current_angle = self.current_angle - (2*PI) 
-        while self.current_angle < (0):
-            self.current_angle = self.current_angle + (2*PI)    
+        self.current_angle += angle    
             
     def calculate_thrust_required(self):
         #PID controller for thrust
@@ -104,28 +100,28 @@ class Vehicle:
         d_gain = 50
         lookahead = 10.0
         #Proportional
-        error_signal = copy.deepcopy(self.current_target)
-        error_signal.sub(self.current_position)      
+        error_signal = self.current_position.dist(self.current_target)     
         proportional_component = error_signal * p_gain
         #Differential
         u = copy.deepcopy(self.current_velocity)
         a = copy.deepcopy(self.current_acceleration)
-        destination = self.current_position + (u.mult(lookahead))
-        destination.sub(self.current_target)
-        derivitive_component = d_gain * (1/lookahead) * destination
-        end_vector = PVector(0.0,0.0)
-        PVector.sub(proportional_component,derivitive_component,end_vector) 
-        end_vector.limit(8)
-        println("At " + str( self.current_position) + ", tgt: " + str(self.current_target) + " Proj dest: " + str(destination) + " thrust vector: " + str(end_vector))
-        return end_vector
+        destination = self.current_position + (u.mult(lookahead))        
+        future_dist_to_target = destination.dist(self.current_target)       
+        derivitive_component = d_gain * (1/lookahead) * future_dist_to_target
+        desired_thrust =  proportional_component - derivitive_component
+        println("At " + str( self.current_position) + ", tgt: " + str(self.current_target) + " Proj dest: " + str(future_dist_to_target) + " thrust: " + str(desired_thrust))
+        if desired_thrust > 8:
+            return 8
+        else:
+            return desired_thrust    
     
-    def apply_thrust(self, thrust_vector):
+    def apply_thrust(self, thrust):
         #Using newton's second law F=ma. 
         #Asssume mass is proportional to size so bigger agents take more effort to turn and start/stop.
         #self.current_acceleration.add(thrust_vector.div(self.mass))
-        effective_thrust_vector = copy.deepcopy(thrust_vector)
-        effective_thrust_vector.rotate(self.current_angle + HALF_PI)
-        self.current_acceleration.add(thrust_vector)
+        effective_thrust_vector = PVector(0,thrust,0)
+        effective_thrust_vector.rotate(self.current_angle - HALF_PI)
+        self.current_acceleration.add(effective_thrust_vector)
         #self.current_acceleration.limit(self.max_acceleration_rate)
     
     def apply_acceleration(self):
@@ -135,50 +131,18 @@ class Vehicle:
     def apply_velocity(self):
         self.current_position.add(self.current_velocity)
     
-    # def increment_current_position(self,current_position,angle,speed):
-    #     x = current_position[0]
-    #     x += speed * cos(angle)
-    #     y = current_position[1]
-    #     y += speed * sin(angle)
-    #     return [x,y]       
-    
-    # def calc_desired_angle(self, current_position, target_position):
-    #       v1 = PVector(current_position[0], current_position[1])
-    #       v2 = PVector(target_position[0],target_position[1])
-    #       return (PVector.angleBetween(v1, v2))
-    
-    # def increment_angle(self, current_angle, desired_angle, max_angular_rate):
-    #     angular_adjustment = desired_angle - current_angle
-    #     if (abs(angular_adjustment)) > max_angular_rate:
-    #         if (angular_adjustment < 0):
-    #             return current_angle - max_angular_rate
-    #         else:
-    #             return current_angle + max_angular_rate
-    #         pass
-    #     else:
-    #         return desired_angle
-    
-    # def checkBorders(self):
-    #     #handle the four extreme corners
-    #     if ((self.current_position[0] < self.border_size) and (self.current_position[1] < self.border_size)):
-    #         self.current_angle += (1.0/2.0) * PI
-    #     elif ((self.current_position[0] < self.border_size) and (self.current_position[1] > self.world_width-self.border_size)):
-    #         self.current_angle += (1.0/2.0) * PI
-    #     elif ((self.current_position[0] > self.world_width-self.border_size) and (self.current_position[1] < self.border_size)):
-    #         self.current_angle += (1.0/2.0) * PI
-    #     elif ((self.current_position[0] > self.world_width-self.border_size) and (self.current_position[1] > self.world_width-self.border_size)):
-    #         self.current_angle += (1.0/2.0) * PI
-            
-    #     #Handle the corders on the four sides
-    #     elif (self.current_position[0] < self.border_size):
-    #         self.current_angle += (1.0/4.0) * PI
-    #     elif (self.current_position[0] > self.world_width-self.border_size):
-    #         self.current_angle += (3.0/4.0) * PI
-    #     elif (self.current_position[1] < self.border_size) :
-    #         self.current_angle += (1.0/4.0) * PI
-    #     elif (self.current_position[1] > self.world_height-self.border_size):
-    #        self.current_angle += (3.0/4.0) * PI
-    #     pass
+
+    def checkBorders(self):
+        #Handle the corders on the four sides
+        if (self.current_position.x < self.border_size):
+            self.current_position.x = self.border_size
+        if (self.current_position.x > (self.world.width_-self.border_size)):
+            self.current_position.x = (self.world.width_-self.border_size)
+        if (self.current_position.y < self.border_size):
+            self.current_position.y = self.border_size
+        if (self.current_position.y > self.world.height_-self.border_size):
+            self.current_position.y = self.world.height_-self.border_size
+        pass
     
     def show(self):
         bottom_left_corner_x = 0 - self.mass
